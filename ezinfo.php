73df46a4f61a84fea63e418db6f73ba9c3f03900
Brookins Consulting <info@brookinsconsulting.com>
2009-06-29 01:24:02 +0000

    Updated: Copyright information in ezinfo file.

--- ezinfo.php
+++ ezinfo.php
@@ -1,15 +1,16 @@
 <?php
 class bcgmapslocationInfo
 {
-    function info()
+    function info( )
     {
+	$BCCurrentYearString = date('Y');
         return array(
-            'Name' => "<a href="http://projects.ez.no/bcgmapslocation">BC GMaps Location</a>",
-            'Version' => "1.0.0",
-            'Copyright' => "Copyright (C) 2009 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
-            'Author' => "Brookins Consulting",
-            'License' => "GNU General Public License v2.0 (or later)"
-        );
+	    'Name' => "<a href='http://projects.ez.no/bcgmapslocation'>BC GMaps Location</a>",
+            'Version' => '1.0.0',
+            'Copyright' => "Copyright (C) 2008 - $BCCurrentYearString <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
+            'License' => "GNU General Public License v2.0 (or later)",
+            'Author' => "Brookins Consulting"
+	    );
     }
 }
 ?>
